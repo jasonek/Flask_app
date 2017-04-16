49b0bd8da1afc1356c55dea7a9609feef13c89b2
Jason Kucharski <jasonok@protonmail.ch>
2017-04-16 12:30:56 -0700

    remove user route and pass wtform to index action

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import *
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'supersecret'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,6 +26,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 if __name__ == '__main__':
     manager.run()
