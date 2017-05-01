cf008de8a1a98e9f6f23d94591affc272d9354aa
Jason Kucharski <jasonok@protonmail.ch>
2017-05-01 05:05:28 -0700

    add a logout route to views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,3 +15,11 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for('main.index'))
