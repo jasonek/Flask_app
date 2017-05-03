257b82ea948b42fd8adb5e28a5b360efa4ca1fd6
Jason Kucharski <jasonok@protonmail.ch>
2017-05-03 04:42:14 -0700

    add confirmation token infrastructure to registering new users

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
